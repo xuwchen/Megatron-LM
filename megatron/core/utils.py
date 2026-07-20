7f323bf682ae03a1faba029d1e20a0ab8d1e797c
xuwenc <xuwenc@nvidia.com>
2026-07-19 20:34:49 -0700

    fix(dist-ckpt): preserve FSDP2 shard identity
    Signed-off-by: xuwenc <xuwenc@nvidia.com>

--- megatron/core/utils.py
+++ megatron/core/utils.py
@@ -1015,7 +1015,8 @@ def make_tp_sharded_tensor_for_checkpoint(
 
     new_offsets.append((tp_axis + prepend_axis_num, tp_rank, tp_size))
 
-    if HAVE_DTENSOR and isinstance(tensor, DTensor):
+    is_data_parallel_fully_sharded = HAVE_DTENSOR and isinstance(tensor, DTensor)
+    if is_data_parallel_fully_sharded:
         # TP + FSDP2 sharding
         dp_replica_id = 0
         tensor = tensor._local_tensor
@@ -1032,7 +1033,7 @@ def make_tp_sharded_tensor_for_checkpoint(
     if replica_id is None:
         replica_id = (0, 0, dp_replica_id)
 
-    return ShardedTensor.from_rank_offsets(
+    sharded_tensor = ShardedTensor.from_rank_offsets(
         key,
         tensor,
         *prepend_offsets,
@@ -1041,6 +1042,9 @@ def make_tp_sharded_tensor_for_checkpoint(
         prepend_axis_num=prepend_axis_num,
         **kwargs,
     )
+    if is_data_parallel_fully_sharded:
+        sharded_tensor.is_data_parallel_fully_shard = True
+    return sharded_tensor
 
 
 def make_sharded_tensor_for_checkpoint(tensor, key, prepend_offsets=(), replica_id=None, **kwargs):
@@ -1082,7 +1086,8 @@ def make_sharded_tensor_for_checkpoint(tensor, key, prepend_offsets=(), replica_
     dp_size = get_pg_size(dp_cp_group)
     dp_replica_id = get_pg_rank(dp_cp_group)
 
-    if HAVE_DTENSOR and isinstance(tensor, DTensor):
+    is_data_parallel_fully_sharded = HAVE_DTENSOR and isinstance(tensor, DTensor)
+    if is_data_parallel_fully_sharded:
         # FSDP2 sharding
         dp_replica_id = 0
         tensor = get_full_tensor_if_necessary(tensor)
@@ -1091,7 +1096,7 @@ def make_sharded_tensor_for_checkpoint(tensor, key, prepend_offsets=(), replica_
     if replica_id is None:
         replica_id = (0, get_pg_rank(tp_group), dp_replica_id)
 
-    return ShardedTensor.from_rank_offsets(
+    sharded_tensor = ShardedTensor.from_rank_offsets(
         key,
         tensor,
         *prepend_offsets,
@@ -1100,6 +1105,9 @@ def make_sharded_tensor_for_checkpoint(tensor, key, prepend_offsets=(), replica_
         prepend_axis_num=prepend_axis_num,
         **kwargs,
     )
+    if is_data_parallel_fully_sharded:
+        sharded_tensor.is_data_parallel_fully_shard = True
+    return sharded_tensor
 
 
 def get_full_tensor_if_necessary(tensor):
